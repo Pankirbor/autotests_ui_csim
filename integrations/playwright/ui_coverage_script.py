4e72323858c20cb325d818dd4655bf8530008a12
Кирилл Панов <lliril_05@list.ru>
2025-09-09 19:03:17 +0300

    Добавлена функция для интеграции скрипта оценки покрытия на страницы сайта

---
+++ integrations/playwright/ui_coverage_script.py
@@ -0,0 +1,28 @@
+from playwright.sync_api import Page, Request, Route
+
+
+def add_coverage_script(page: Page):
+    """Динамически добавляет скрипт UI Coverage в каждую страницу"""
+
+    def inject_coverage_script(route: Route, request: Request):
+        if not request.resource_type == "document":
+            route.continue_()
+            return
+
+        response = request.response()
+        if not response:
+            route.continue_()
+            return
+
+        html_content = response.text()
+        modified_html_content = html_content.replace(
+            "</body>",
+            "<script src='https://nikita-filonov.github.io/ui-coverage-report/agent.global.js'></script></body>",
+        )
+        route.fulfill(
+            status=response.status,
+            headers=response.headers,
+            body=modified_html_content.encode("utf-8"),
+            content_type="text/html",
+        )
+        page.route("**/*", inject_coverage_script)
