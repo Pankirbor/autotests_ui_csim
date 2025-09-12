ec9d3dd5fae2268e34c634dadce8f0a4a738dff9
Кирилл Панов <lliril_05@list.ru>
2025-09-12 12:53:36 +0300

    Добавлены проверки активности элемента Link и Tab

--- src/ui/elements/link.py
+++ src/ui/elements/link.py
@@ -1,4 +1,13 @@
+from typing import Self
+
+import allure
+from playwright.sync_api import expect
+from ui_coverage_tool import ActionType
+
 from src.ui.elements.base_element import BaseElement
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__.upper())
 
 
 class Link(BaseElement):
@@ -6,4 +15,36 @@ class Link(BaseElement):
     Класс превставления ссылки на веб-странице.
     """
 
-    pass
+    def check_enabled(self, nth: int = 0, **kwargs) -> Self:
+        """
+        Проверяет, что ссылка активна (доступна для нажатия).
+
+        Аргументы:
+            nth (int): Индекс элемента, если на странице несколько одинаковых ссылок.
+            **kwargs: Дополнительные параметры для форматирования локатора.
+        """
+        step = f"Проверка, что {self.type_of} '{self.name}' активна"
+        with allure.step(step):
+            locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
+            expect(locator).to_be_enabled()
+
+        self.track_coverage(ActionType.ENABLED, nth, **kwargs)
+        return self
+
+    def check_disabled(self, nth: int = 0, **kwargs) -> Self:
+        """
+        Проверяет, что ссылка неактивна (недоступна для нажатия).
+
+        Аргументы:
+            nth (int): Индекс элемента, если на странице несколько одинаковых ссылок.
+            **kwargs: Дополнительные параметры для форматирования локатора.
+        """
+        step = f"Проверка, что {self.type_of} '{self.name}' неактивна"
+        with allure.step(step):
+            locator = self.get_locator(nth, **kwargs)
+            logger.info(step)
+            expect(locator).to_be_disabled()
+
+        self.track_coverage(ActionType.DISABLED, nth, **kwargs)
+        return self
--- src/ui/elements/tab.py
+++ src/ui/elements/tab.py
@@ -1,9 +1,47 @@
+import re
+
+import allure
+from ui_coverage_tool import ActionType
+from playwright.sync_api import expect
+
 from src.ui.elements.base_element import BaseElement
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__.upper())
 
 
 class Tab(BaseElement):
     """
-    Класс превставления текста на веб-странице.
+    Класс превставления элемента таба на веб-странице.
     """
 
-    pass
+    def is_active(self, nth: int = 0, **kwargs) -> bool:
+        """
+        Проверяет, что элемент таба является активным (имеет класс 'q-tab--active').
+
+        Args:
+            nth (int): Индекс элемента, если на странице несколько одинаковых элементов.
+            **kwargs: Дополнительные параметры для форматирования локатора.
+
+        Returns:
+            bool: True, если таб активен, иначе False.
+        """
+        step_description = (
+            f"Проверка: таб '{self.name}' активен (имеет класс 'q-tab--active')."
+        )
+
+        with allure.step(step_description):
+            locator = self.get_locator(nth, **kwargs)
+
+            try:
+                logger.info(f"{step_description}")
+                expect(locator).to_have_class(
+                    re.compile(r"q-tab--active"), timeout=5000
+                )
+                is_selected = True
+            except AssertionError:
+                is_selected = False
+                logger.warning(f"⚠️ {step_description} — таб НЕ активен.")
+
+        self.track_coverage(ActionType.CHECKED, nth, **kwargs)
+        return is_selected
