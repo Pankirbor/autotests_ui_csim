dd7bf466168ae60e0f3a9409cd49a32ca8083e2e
Кирилл Панов <lliril_05@list.ru>
2025-09-12 12:44:02 +0300

    Изменен атрибут получения табов в FilterVacanciesComponent на объект свойство

--- src/ui/components/filters/filter_component.py
+++ src/ui/components/filters/filter_component.py
@@ -1,33 +1,53 @@
 from typing import Self
-from playwright.sync_api import Page
+import allure
+from playwright.sync_api import Page, expect
 
 from src.ui.components.base_component import BaseComponent
 from src.ui.elements.button import Button
 from src.ui.elements.container import Container
+from src.ui.elements.icon import Icon
 from src.ui.elements.tab import Tab
 from src.ui.locators import FilterVacanciesLocators
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__.upper())
 
 
 class FilterVacanciesComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.tabs = self._get_filters_tabs()
         self.container = Container.by_xpath(
             page, *FilterVacanciesLocators.FILTER_CONTAINER
         )
+        self.tab_all = Tab(page, *FilterVacanciesLocators.TAB_ALL_VACANCIES)
         self.sort_btn = Button.by_xpath(page, *FilterVacanciesLocators.SORT)
+        self.up_icon = Icon.by_xpath(page, *FilterVacanciesLocators.ICON_SORTING_UP)
+        self.down_icon = Icon.by_xpath(page, *FilterVacanciesLocators.ICON_SORTING_DOWN)
         self.filter_menu = Button.by_xpath(page, *FilterVacanciesLocators.FILTER_BTN)
 
-    def should_be_visible(self) -> Self:
+    @property
+    def tabs(self) -> list[Tab]:
+        """Ленивое свойство: возвращает список вкладок при каждом обращении."""
+        return self._get_filters_tabs()
+
+    @allure.step("Проверка видимости фильтра")
+    def check_visible(self) -> Self:
         self.container.check_visible()
         return self
 
     def _get_filters_tabs(self) -> list[Tab]:
         xpath, name = FilterVacanciesLocators.TAB
-        return [
-            Tab(self.page, xpath.format(index=i), name.format(title=item.inner_text()))
-            for i, item in enumerate(
-                self.page.locator(FilterVacanciesLocators.ALL_TABS).all()
-            )
-        ]
+        tab_elements = self.page.locator(FilterVacanciesLocators.ALL_TABS).all()
+        # tab_elements = tabs.all()
+
+        result = []
+        for i, item in enumerate(tab_elements, start=1):
+            tab_text = item.inner_text().strip()
+            if tab_text != "Все":
+                result.append(
+                    Tab.by_xpath(
+                        self.page, xpath.format(index=i), name.format(title=tab_text)
+                    )
+                )
+        return result
