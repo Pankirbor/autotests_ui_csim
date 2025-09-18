84d134941e695de82fb5343b1557046bc2302a8a
Кирилл Панов <lliril_05@list.ru>
2025-09-18 13:37:49 +0300

    Внесены корректировки в логирование html captcha метода visit() BasePage3

--- src/ui/pages/base_page.py
+++ src/ui/pages/base_page.py
@@ -37,7 +37,9 @@ class BasePage:
     def accept_cookies_if_present(self) -> None:
         """Принимает куки, если диалог виден."""
 
-        logger.warning(f"{self.page.locator('//body').inner_html()[:1000]}")
+        header = self.page.locator("//header").first
+        logger.warning(f"HEADER HTML BEFORE ACCEPT COOKIES: {header.inner_html()}")
+
         cookies_dialog = self.page.locator(CookiesLocators.CONTAINER.selector)
         accept_button = self.page.locator(CookiesLocators.ACCEPT_BUTTON.selector)
 
@@ -65,7 +67,8 @@ class BasePage:
                 logger.warning(f"⚠️ Обнаружена CAPTCHA-страница на URL: {self.page.url}")
                 logger.warning(f"📄 Заголовок страницы: {self.page.title()}")
                 logger.warning(f"Ошибка {type(e).__name__}: {e}")
-                logger.warning(f"{self.page.locator('//body').inner_html()[:500]}")
+                header = self.page.locator("//header").first
+                logger.warning(f"HEADER HTML: {header.inner_html()}")
                 artifact_dir = Path("artifacts/captcha")
 
                 html_files = list(artifact_dir.glob("*.html"))
