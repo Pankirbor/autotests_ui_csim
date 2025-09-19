0b1e7760f89db6a52d013914f54bb0ed3d0c3921
Кирилл Панов <lliril_05@list.ru>
2025-09-19 13:03:31 +0300

    Добавлен общий метод проверки загрузки файла для элемента Link

--- src/ui/elements/link.py
+++ src/ui/elements/link.py
@@ -1,7 +1,7 @@
 from typing import Self
 
 import allure
-from playwright.sync_api import expect
+from playwright.sync_api import expect, Download
 from ui_coverage_tool import ActionType
 
 from src.ui.elements.base_element import BaseElement
@@ -44,3 +44,33 @@ class Link(BaseElement):
 
         self.track_coverage(ActionType.DISABLED, nth, **kwargs)
         return self
+
+    def check_download_file(self, file_name: str, nth: int = 0, **kwargs) -> Download:
+        """Скачивает файл по ссылке.
+
+        Аргументы:
+            nth (int): Индекс элемента, если на странице несколько одинаковых ссылок.
+            **kwargs: Дополнительные параметры для форматирования локатора.
+        """
+        with self.page.expect_download() as download_info:
+            self.click(nth, **kwargs)
+
+        download: Download = download_info.value
+        downloaded_file_name = download.suggested_filename
+        logger.info(f"Файл '{downloaded_file_name}' загружен")
+
+        check_file_name_step = (
+            f"Проверяем соответствие имени файла ожидаемому '{file_name}'"
+        )
+        with allure.step(check_file_name_step):
+            logger.info(check_file_name_step)
+            assert (
+                file_name == downloaded_file_name
+            ), f"Название '{downloaded_file_name}' не соответствует ожидаемому '{file_name}'"
+
+        is_empty_file_step = f"Проверяем, что файл '{downloaded_file_name}' не пустой"
+        with allure.step(is_empty_file_step):
+            logger.info(is_empty_file_step)
+            file_path = download.path()
+            file_size = file_path.stat().st_size
+            assert file_size > 0, f"Скачанный файл '{downloaded_file_name}' пустой"
