d979b4f45c40365bf0f84c6d85a0892a3a71da22
Кирилл Панов <lliril_05@list.ru>
2025-09-19 13:01:29 +0300

    Изменен тег для AllureEpic на более глобальный уровень.

--- integrations/allure/epic.py
+++ integrations/allure/epic.py
@@ -4,5 +4,4 @@ from enum import Enum
 class AllureEpic(str, Enum):
     """Enum, представляющий эпики в виде строковых перечислений."""
 
-    VACANCIES = "Страница вакансий"
-    NAVIGATION = "Навигация по сайту"
+    CSIM = "Сайт ЦСИМ"
